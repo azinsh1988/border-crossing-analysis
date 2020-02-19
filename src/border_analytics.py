dd8e3e5589d2575198fe4ddda39957129dead156
Azin Shamshirgaran <36013491+azinsh1988@users.noreply.github.com>
2020-02-19 09:28:28 -0500

    Update border_analytics.py

--- src/border_analytics.py
+++ src/border_analytics.py
@@ -1,1 +1,21 @@
+def myOpenCSV(csv_file_directory, date_index=0):
+    with open(csv_file_directory, 'r') as f:
+        #read from csv line by line, rstrip helps to remove '\n' at the end of line
+        lines = [line.rstrip() for line in f] 
+
+        results = []
+
+        # skip the header by starting from index 1
+        for line in lines[1:]:
+            words = line.split(',') # get each item in one line
+            date = words[date_index][1:-1] # remove ("") from date
+            results.append([date] + words[:date_index] + words[date_index+1:])
+        
+        # sort the results based on date
+        results.sort()
+        return results
+      
+if __name__ == '__main__':
+    try:
+        myOpenCSV(csv_file_directory, date_index=0)
 
