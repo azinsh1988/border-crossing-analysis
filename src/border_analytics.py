e5762e5b4ecd007170a626cc160b71f22b36ed8b
Azin Shamshirgaran <36013491+azinsh1988@users.noreply.github.com>
2020-02-29 12:16:02 -0500

    Update border_analytics.py

--- src/border_analytics.py
+++ src/border_analytics.py
@@ -1,23 +1,15 @@
 def myOpenCSV(csv_file_directory, date_index=4):
     
     with open(csv_file_directory, 'r') as f:
-        #read the csv file line by line
         lines = [line.rstrip().split(",") for line in f]
-        
-        # remove the header
         header = lines[0]
-        
-        # skip the header by starting from index 1
         results = lines[1:]
         
-        
-        # sort the results based on date
+
         results.sort(key=lambda x: x[date_index][6:10]+x[date_index][0:2])
-        print("Data is ready for analysis.")
         return header,results
 
 def myGroupBy(lst, date_index=4, border_index=3, measure_index=5, value_index=6):
-    print("Groupby date borde and measure ...")
             
     border_date_measure_MAP = {}
     border_measure_MAP = {}
@@ -48,23 +40,22 @@ def myGroupBy(lst, date_index=4, border_index=3, measure_index=5, value_index=6)
         
         
         border_measure_MAP[key2] = border_measure_MAP.get(key2, 0)+value
-    print("Groupby is done.")
+   
 
     return border_date_measure_MAP
 
 def MyDicToList(dic, border_index=0, date_index=1, measure_index=2, value_index=3):
-    print("Converting dic to list ...")
+    
     res = []
     for k,v in dic.items():
         A = k.split("_")
         A+=v
         res+=A,
     res.sort(key = lambda x: (x[date_index][6:10]+x[date_index][:2],x[value_index],x[measure_index],x[border_index]), reverse = True)
-    print("your results are ready.")
     return res
 
 def MyToCSV(lst, location = "./output/report.csv", header = ["Border","Date","Measure","Value","Average"]):
-    print("Writing to csv ...")
+    
     lst = [header]+lst
     with open(location,'w') as file:
         for line in lst:
@@ -72,7 +63,7 @@ def MyToCSV(lst, location = "./output/report.csv", header = ["Border","Date","Me
                 file.write(str(c)+',')
             file.write(str(line[-1]))
             file.write('\n')
-    print("Your CSV file is ready!")
+    
 
 
 if __name__ == '__main__':
